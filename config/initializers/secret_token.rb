6d6d16a54db894c5567933cf798219f90d1657f0
Alex Antonov <admin@asiniy.com>
2012-03-12 18:38:00 +0400

    Git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialclient::Application.config.secret_token = 'e35b9bf721eec169002d71f1f280ad992c45ce21e5169244b57ea4e3f5a1411d23607278e739cc5a30f19a3005a7ec66b33b3516b4ce6e2eeb49c89b71724bef'
